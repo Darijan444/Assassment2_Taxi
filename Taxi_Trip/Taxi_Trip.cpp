d327305a7a6d085c1e69905240af85b5610137bf
Darijan444 <darijan444@gmail.com>
2022-06-08 12:41:07 +0200

    first commit

---
+++ Taxi_Trip/Taxi_Trip.cpp
@@ -0,0 +1,20 @@
+// Taxi_Trip.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter your name\n";
+
+    cout << "I'm Ryuu\n";
+
+    cout << "I'm Darijan\n";
+
+    cout << "I'm Veni\n";
+
+    return 0;
+}
+
+
